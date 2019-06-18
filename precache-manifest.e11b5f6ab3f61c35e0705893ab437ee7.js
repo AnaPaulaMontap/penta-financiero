34b712335302cab52627807fe40cf748165dcc7d
AnaPaulaMOntap <anapaula.monsalvet@gmail.com>
2019-06-18 18:15:10 -0400

    Updates

---
+++ precache-manifest.e11b5f6ab3f61c35e0705893ab437ee7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "70ad31bc512bc69caa98c3f71ebc9b4d",
+    "url": "/penta-financiero/index.html"
+  },
+  {
+    "revision": "48778917cff68c132ba4",
+    "url": "/penta-financiero/static/css/main.e4d1ddca.chunk.css"
+  },
+  {
+    "revision": "7ea97bab8b0e15266874",
+    "url": "/penta-financiero/static/js/2.a3f445e3.chunk.js"
+  },
+  {
+    "revision": "48778917cff68c132ba4",
+    "url": "/penta-financiero/static/js/main.112dc7c1.chunk.js"
+  },
+  {
+    "revision": "468494697a27a663e2c3",
+    "url": "/penta-financiero/static/js/runtime~main.326d7f88.js"
+  },
+  {
+    "revision": "a2ad4b168a50dbecfa9d11c6cc0c8e52",
+    "url": "/penta-financiero/static/media/Marcador de texto.a2ad4b16.png"
+  }
+]);
